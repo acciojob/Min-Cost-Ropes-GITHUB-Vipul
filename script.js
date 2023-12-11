09004b862efec9b277780e2fe2f8384e8cc50045
GITHUB-Vipul <126069867+GITHUB-Vipul@users.noreply.github.com>
2023-12-11 15:15:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
+function comparable(a,b)
+	{
+		if (a > b) return 1;
+   if (a == b) return 0;
+   if (a < b) return -1;
+	}
 function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+	let min_cost=0;
+	arr.sort(comparable)
+	while(arr.length>0)
+		{
+			let num1=arr.shift();
+			let num2=arr.shift();
+			let cost=num1+num2;
+			min_cost+=cost;
+			arr.push(cost);
+			arr.sort(comparable);
+		}
+  return min_cost;
 }
 
 module.exports=mincost;
